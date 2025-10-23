4146d04699c0cb4f3cdc891a46b70afe8899d494
Hounaida-ali <hounaidaalibilal057@gmail.com>
2025-10-23 11:31:11 +0100

    feat:implementation des routes

--- frontend/gestion_scolarite/src/app/app.routes.ts
+++ frontend/gestion_scolarite/src/app/app.routes.ts
@@ -5,15 +5,26 @@ import { Login } from './components/login/login';
 import { Reinisilize } from './components/reinisilize/reinisilize';
 import { ResetPassword } from './components/reset-password/reset-password';
 import { VerifyMail } from './components/verify-mail/verify-mail';
+import { Contact } from './components/contact/contact';
+import { formation } from './components/formation/formation';
+import { Program } from './components/program/program';
+import { APropos } from './components/a-propos/a-propos';
+import { Ressource } from './components/ressource/ressource';
+import { Calendar } from './components/calendar/calendar';
+import { SeeAllDashboard } from './components/see-all-dashboard/see-all-dashboard';
+import { AllNews } from './components/all-news/all-news';
 
 export const routes: Routes = [
     { path:'',component:Acceuil},
     { path:'registre',component:Registre},
-    { path:'login',component:Login},
+    // { path:'login',component:Login},
     { path:'reinisilize',component:Reinisilize},
     { path:'resetpassword',component:ResetPassword},
     { path:'verifyemail',component:VerifyMail},
-
-
-
+    { path:'contact',component:Contact},
+    { path:'a-propos',component:APropos},
+    { path:'formation',component:formation},
+    { path:'programme',component:Program},
+    { path:'ressource',component:Ressource},
+    { path:'calendar',component:Calendar},
 ];
