d99126f88d4e3ab4885745f774018ef8d8001b7e
Hounaida-ali <hounaidaalibilal057@gmail.com>
2025-10-23 13:50:00 +0100

    generer un service evenement

---
+++ frontend/gestion_scolarite/src/app/services/event-service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Event } from '../interfaces/eventInterface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+   private apiUrl = 'http://localhost:5000/api/events';
+
+  constructor(private http: HttpClient) { }
+
+  getEvents(academicYear?: string, type?: string, semester?: number): Observable<Event[]> {
+    let params = new HttpParams();
+    if (academicYear) params = params.set('academicYear', academicYear);
+    if (type) params = params.set('type', type);
+    if (semester) params = params.set('semester', semester.toString());
+
+    return this.http.get<Event[]>(this.apiUrl, { params });
+  }
+
+  getEvent(id: string): Observable<Event> {
+    return this.http.get<Event>(`${this.apiUrl}/${id}`);
+  }
+
+  createEvent(event: Event): Observable<Event> {
+    return this.http.post<Event>(this.apiUrl, event);
+  }
+
+  updateEvent(id: string, event: Event): Observable<Event> {
+    return this.http.put<Event>(`${this.apiUrl}/${id}`, event);
+  }
+
+  deleteEvent(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  getEventsByAcademicYear(academicYear: string): Observable<Event[]> {
+    return this.http.get<Event[]>(`${this.apiUrl}/year/${academicYear}`);
+  }
+  
+  getUpcomingEvents(): Observable<Event[]> {
+  return this.http.get<Event[]>(`${this.apiUrl}/upcoming/next`);
+}
+}
