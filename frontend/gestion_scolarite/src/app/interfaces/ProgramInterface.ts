1acb4171faeed215ba388b6b9d92f9042a492062
Hounaida-ali <hounaidaalibilal057@gmail.com>
2025-10-23 13:34:39 +0100

    fet:generation d'interface programme et actualite du departement

---
+++ frontend/gestion_scolarite/src/app/interfaces/ProgramInterface.ts
@@ -0,0 +1,16 @@
+import { Niveau } from './niveauInterface';
+
+export interface Programme {
+  _id?: string;
+  departement: string; // eco, gestion, droit
+  code: string; // sciences-eco, monetaire-bancaire, admin-gestion, droit-affaires
+  nom: string;
+  description: string;
+  duree: string;
+  credits: string;
+  diplome: string;
+  acces: string;
+  niveaux: Niveau[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
---
+++ frontend/gestion_scolarite/src/app/interfaces/actualiteInterface.ts
@@ -0,0 +1,7 @@
+export interface News {
+    _id?: string;
+    titre: string;
+    contenu: string;
+    date: string;
+    actionText: string;
+}
