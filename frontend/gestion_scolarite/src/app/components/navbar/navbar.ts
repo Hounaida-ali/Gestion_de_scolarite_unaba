e66415d86894f57e73d33957b59bacfd5b7ac772
Hounaida-ali <hounaidaalibilal057@gmail.com>
2025-10-23 11:21:52 +0100

    feat:implementation du navbar

--- frontend/gestion_scolarite/src/app/components/navbar/navbar.ts
+++ frontend/gestion_scolarite/src/app/components/navbar/navbar.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { AuthModal } from '../../services/auth-modal';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
 })
 export class Navbar {
+  constructor(private authModalService: AuthModal) {}
 
+  openLoginModal(): void {
+    this.authModalService.openLoginModal();
+  }
+
+  // Ouvre la modal de Register
+  openRegisterModal(): void {
+    this.authModalService.openRegisterModal();
+  }
 }
